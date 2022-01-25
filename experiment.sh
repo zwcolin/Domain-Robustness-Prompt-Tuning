7eabfa048a6af29f43678c6e54a864be889c048a
rachelluoyt <y5luo@ucsd.edu>
2022-01-24 18:46:07 -0800

    run

--- experiment.sh
+++ experiment.sh
@@ -1,8 +1,9 @@
 nohup python run.py \
     --method prompt_tuning \
-    --model t5-small \
-    --n_tokens 10 \
+    --model t5-base \
+    --n_tokens 20 \
     --mode train \
+    --bz 128 \
     --dataset squad &\
 # more arguments can be added
 
