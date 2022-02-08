dc32a9f992a528332477aa74dc4a3e2452d2d8a5
wanglec <l6wang@ucsd.edu>
2022-02-07 23:56:40 -0800

    clean code and dataset

--- engine_prefix_tuning.py
+++ engine_prefix_tuning.py
@@ -152,10 +152,10 @@ def run(args):
         print("running evaluation on ", checkpoint_path)
 
         if args["test_set"] == "webnlg":
-            print("python gen.py webnlg yes valid {} no".format(checkpoint_path))
-            # print("python gen.py webnlg yes test {} no".format(checkpoint_path))
-            os.system("python gen.py webnlg yes valid {} no".format(checkpoint_path))
-            # os.system("python gen.py webnlg yes test {} no".format(checkpoint_path))
+            # print("python gen.py webnlg yes valid {} no".format(checkpoint_path))
+            print("python gen.py webnlg yes test {} no".format(checkpoint_path))
+            # os.system("python gen.py webnlg yes valid {} no".format(checkpoint_path))
+            os.system("python gen.py webnlg yes test {} no".format(checkpoint_path))
 
         elif args["test_set"] == "dart":
             print("python gen.py triples yes test {} no".format(checkpoint_path))
